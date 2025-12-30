3b15aa3ef2ef44be6bd00f36d09f462d01e5802e
kanai <kanaislouis@gmail.com>
2025-12-30 18:17:03 +0100

    feat(prediction): initialize ensemble training function and features

--- quant_a/prediction.py
+++ quant_a/prediction.py
@@ -5,3 +5,14 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import TimeSeriesSplit
 from xgboost import XGBClassifier
 from quant_a.indicators import compute_technical_indicators
+
+def train_predict_ensemble(df, ticker):
+    # Retrieve technical features from our previous module
+    data = compute_technical_indicators(df, ticker)
+    
+    # Define features (X) and target (y)
+    features = ['Dist_MA20', 'RSI', 'BB_Width', 'Hist_Vol']
+    X = data[features]
+    y = data['Target']
+    
+    return X, y
